bf75ec8c64a5018c0ac50d94c7bf095d080a2c8f
Roger Gabiru <roger.gabiru@gmail.com>
2023-09-05 16:50:36 -0300

    update .dist

---
+++ src/js/checkout.js
@@ -0,0 +1,47 @@
+import {
+	desenharProdutoNoCarrinhoSimples,
+	lerLocalStorage,
+	apagarDoLocalStorage,
+	salvarLocalStorage,
+} from "./utilidades";
+
+import { atualizarPrecoCarrinho } from "./menuCarrinho";
+
+function desenharProdutosCheckout() {
+	const idsProdutoCarrinhoComQuantidade = lerLocalStorage("carrinho") ?? {};
+
+	for (const idProduto in idsProdutoCarrinhoComQuantidade) {
+		desenharProdutoNoCarrinhoSimples(
+			idProduto,
+			"container-produtos-checkout",
+			idsProdutoCarrinhoComQuantidade[idProduto]
+		);
+	}
+
+	atualizarPrecoCarrinho();
+}
+
+function finalizarCompra(evento) {
+	evento.preventDefault();
+	const idsProdutoCarrinhoComQuantidade = lerLocalStorage("carrinho") ?? {};
+	if (Object.keys(idsProdutoCarrinhoComQuantidade).length === 0) {
+		return;
+	}
+
+	const dataAtual = new Date();
+	const pedidoFeito = {
+		dataPedido: dataAtual,
+		pedido: idsProdutoCarrinhoComQuantidade,
+	};
+	const historicoDePedidos = lerLocalStorage("historico") ?? [];
+	const historicoDePedidosAtualizado = [pedidoFeito, ...historicoDePedidos];
+
+	salvarLocalStorage("historico", historicoDePedidosAtualizado);
+
+	apagarDoLocalStorage("carrinho");
+	window.location.href = "./pedidos.html";
+}
+
+desenharProdutosCheckout();
+
+document.addEventListener("submit", (evt) => finalizarCompra(evt));
--- src/js/vite.config.js
+++ src/js/vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	base: "/ecommerceJS/",
+	base: "/gabirulivre/",
 	plugins: [],
 	resolve: {
 		/*something*/
